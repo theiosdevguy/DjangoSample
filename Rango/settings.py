3dd3883d0c6aa99b5d79b09f187a167f2b132775
Ankit Gupta <ankit.gupta619@gmail.com>
2015-12-26 21:06:16 +0530

    Static root add

--- Rango/settings.py
+++ Rango/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_URL = '/static/'
